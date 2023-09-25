15290411ddac27570ed7adacb307a954120f70f0
Tim <timmaxgibson@gmail.com>
2023-09-25 14:47:14 +1000

    updates

--- build.bash
+++ build.bash
@@ -1,2 +1,12 @@
 #!/bin/bash
-docker build --build-arg="HOST_USER=$(whoami)" -f ./docker/Dockerfile.ros -t uts-ros .
+docker build \
+  --build-arg="HOST_USER=$(whoami)" \
+  --build-arg="DISPLAY=$(echo $DISPLAY)" \
+  -f ./docker/Dockerfile.ros \
+  -t uts-ros-test .
+
+docker build \
+  --build-arg="HOST_USER=$(whoami)" \
+  --build-arg="DISPLAY=$(echo $DISPLAY)" \
+  -f ./docker/Dockerfile.matlab \
+  -t uts-matlab-test .
